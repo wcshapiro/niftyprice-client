429e8480af27f6e7cd3edc4f8f9341a03524bba9
Will Shapiro <willshapiro@will-2.local>
2021-09-24 19:39:32 -0700

    changed footer to be responsive

--- src/Footer.js
+++ src/Footer.js
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import "./Footer.css"
-import { Button } from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import "./Footer.css";
+import { Button } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
-function useForceUpdate(){
-    const [value, setValue] = useState(0); // integer state
-    return () => setValue(value => value + 1); // update the state to force render
+function useForceUpdate() {
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue((value) => value + 1); // update the state to force render
 }
 function Footer() {
-    const forceUpdate = useForceUpdate();
-    return (
-        <div class="footer" >
-            <hr></hr>
-            <div>
-            <div class="left-desc">
-                <h3 class="content-title">About</h3>
-                <div class="about-content">
-                    <a class = "link" href="/about">About Us</a>
-                    <a class = "link" href="/purchase">Pricing</a>
-                </div>
-            </div>
-            <div class="mid-desc">
-                <h3 class="content-title">Contact</h3>
-                <div class="contact-content">
-                    <p >Email: willcshapiro@gmail.com</p>
-                </div>
-            </div>
-            <div class="right-desc">
-                <h3 class="content-title">Links</h3>
-                <div class="links-content">
-                    <a href="https://twitter.com/niftyprice_io" id="twitter"></a>
-                </div>
-
-            </div>
-            </div>
-            <div class="copy">© 2021 | niftyprice.io</div>
-
-        </div>
-    )
-
-} export default Footer;
+  const forceUpdate = useForceUpdate();
+  return (
+    <div class="footer">
+      <hr></hr>
+      <Grid container justifyContent="space-evenly">
+        <Grid item lg={4} sm={6} xs={12}>
+          <Typography variant="h4" component="h4" align="center">
+            About
+          </Typography>
+          <Typography variant="h7" component="h7" align="center">
+            <a class="link" href="/about">
+              About Us
+            </a>
+          </Typography>
+          <Typography variant="h7" component="h7" align="center">
+            <a class="link" href="/purchase">
+              Pricing
+            </a>
+          </Typography>
+          <div class="about-content"></div>
+        </Grid>
+        <Grid item lg={4} sm={6} xs={12}>
+          <Typography variant="h4" component="h4" align="center">
+            Contact
+          </Typography>
+          <Typography variant="h7" component="h7" align="center">
+            <p>Email: willcshapiro@gmail.com</p>
+            <p>© 2021 | niftyprice.io</p>
+          </Typography>
+          <div class="contact-content"></div>
+        </Grid>
+        <Grid item lg={4} sm={6} xs={12}>
+          <Typography variant="h4" component="h4" align="center">
+            Links
+          </Typography>
+          <Typography variant="h7" component="h7" align="center">
+            <a href="https://twitter.com/niftyprice_io" id="twitter"></a>
+          </Typography>
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
+}
+export default Footer;
