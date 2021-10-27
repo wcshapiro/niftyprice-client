007038faca27a26099fb9a628ca62d0c3ce1f736
cpkelley5 <cpkelley5@gmail.com>
2021-10-26 23:02:39 -0400

    Added button to blog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import React from "react";
-import Table from "./Table.js";
-import Charts from "./Charts.js";
-import { AppBar, Toolbar, Button, Grid } from "@material-ui/core";
-import Footer from "./Footer.js";
-import Purchase from "./Purchase.js";
-import About from "./About.js";
-import Privacy from "./Privacy.js";
-import Terms from "./Terms.js";
+import './App.css';
+import React from 'react';
+import Table from './Table.js';
+import Charts from './Charts.js';
+import { AppBar, Toolbar, Button, Grid } from '@material-ui/core';
+import Footer from './Footer.js';
+import Purchase from './Purchase.js';
+import About from './About.js';
+import Privacy from './Privacy.js';
+import Terms from './Terms.js';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -21,11 +21,20 @@ function App() {
               <Grid justify="flex-start" container>
                 <Grid item>
                   <a id="niftyprice" href="/">
-                    {" "}
+                    {' '}
                   </a>
                 </Grid>
               </Grid>
               <Grid container justify="flex-end" spacing={5}>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    href="https://blog.niftyprice.io"
+                  >
+                    Blog
+                  </Button>
+                </Grid>
                 <Grid item>
                   <Button variant="contained" color="primary" href="/about">
                     About Us
